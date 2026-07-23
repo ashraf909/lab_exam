690c9b1771c6c61219650faaabdcd73df44dbb7f
maruf <funbd123456@gmail.com>
2026-07-23 14:24:10 +0600

    new js file added

---
+++ backhand.js
@@ -0,0 +1,8 @@
+// Display a message in the browser console
+console.log("Hello, World!");
+
+// Show an alert box
+alert("Welcome to my website!");
+
+// Change the text of an HTML element
+document.getElementById("title").innerHTML = "Hello, JavaScript!";
